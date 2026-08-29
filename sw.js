33b8262d925f400e59e95014ba3b8573ff5cc81d
Manngez <121944912+Manngez@users.noreply.github.com>
2026-08-29 09:16:55 +0200

    Add offline support

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='ml-gym-v1';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./assets/exercises/benpress.svg','./assets/exercises/brostpress.svg','./assets/exercises/sittande-rodd.svg','./assets/exercises/goblet-squat.svg','./assets/exercises/latsdrag.svg','./assets/exercises/planka.svg','./assets/exercises/larcurl.svg','./assets/exercises/axelpress.svg','./assets/exercises/kabelrodd.svg','./assets/exercises/step-up.svg','./assets/exercises/hoftlyft.svg','./assets/exercises/sidoplanka.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match('./index.html'))))});
